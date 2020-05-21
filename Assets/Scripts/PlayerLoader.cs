b51193da9f8492b23769e4638b2366c1b248323c
rmachadob <48966935+rmachadob@users.noreply.github.com>
2020-05-20 21:39:21 -0300

    Making sure the player is aways on scene

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour {
+
+    //checa se já tem um player na cena, senão spawna um
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		if(PlayerController.instance == null)
+        {
+            Instantiate(player);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
