3a3b84efee69b49f7bb0c21b77c23ba5acc21c23
rmachadob <48966935+rmachadob@users.noreply.github.com>
2020-05-23 19:18:13 -0300

    Keeping Cameras Inside The Map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour {
 
     public Transform target;
-
+    //reference to the tilemap size 
+    public Tilemap theMap;
+    //2 values to store the position of the map
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
 
 	// Use this for initialization
 	void Start () {
         
         target = PlayerController.instance.transform;
+        //That gets from the tile map the minimum amount (furthest left on the x and furthest down on the y)
+        bottomLeftLimit = theMap.localBounds.min;
+
+        //pela lógica contrário uso o max pra pegar o limite de cima e da direita
+        topRightLimit = theMap.localBounds.max;
 	}
 	
 	// LateUpdate is called once per frame after Update
@@ -20,5 +30,14 @@ public class CameraController : MonoBehaviour {
         //the transform.position of the object that this script is attached to       
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);//Vector 3 this time because the transform has the x, y and z positons
         //fot the z axis I get the already existing by transform.position.z
+
+        //keep the camera inside the bounds
+        //Mathf = Math function, o Clamp é pra prender entre dois valores (topRightLimit e bottomLeftLimit)
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), 
+            Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y),
+            transform.position.z);
+        //Como é Vector 3 preciso fazer os 3 eixos o z eu não altero, pego como está 
+
+
     }
 }
