ca11f4f6671c3d3e177bb796f86764de61116f94
rmachadob <48966935+rmachadob@users.noreply.github.com>
2020-05-25 21:08:34 -0300

    Reading Multiple Lines

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -6,7 +6,7 @@ public class DialogManager : MonoBehaviour {
 
     public Text dialogText;//Diálogo 
     public TextAlignment nameTest;//Nome de quem fala
-    public GameObject dalogBox;//para saber se deve mostar ou não na UI
+    public GameObject dialogBox;//para saber se deve mostar ou não na UI
     public GameObject nameBox;
 
     //string[] declarando um array do tipo String
@@ -21,7 +21,33 @@ public class DialogManager : MonoBehaviour {
 	}
 
 	void Update () {
-		
+
+        //we can only do this if the dialog box is open
+        if (dialogBox.activeInHierarchy)    //is this active at the scene at the moment?
+
+        //--------------além do getAxisRaw eu tenho tambem o metodo getbutton disponiveis pela classe Input------------------------
+        //-------------GetButton significa: o botão está sendo apertado? se o player segurar o botão ele skipa todo o diálogo de uma vez---------------------
+
+
+        {//GetButtonUp só pega quando aperta
+            if (Input.GetButtonUp("Fire1"))//Edit-ProjectSettings-Input(menu com os botoes para mapeamento, estou usando o Fire1 aqui(olhar classe playerController)
+            {
+                currentLine++;//currentLine sempre pega a proxima entao uso ele como i
+
+
+                if (currentLine >= dialogLines.Length)///if para não estourar o meu array
+                {
+                    dialogBox.SetActive(false);//desativo o dialogo
+
+                }
+                else
+                {
+                dialogText.text = dialogLines[currentLine];
+
+                }
+
+            }
+        }
 
 	}
 }
